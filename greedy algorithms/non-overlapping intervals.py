55da56cbff65399cb0fe6ae3b78668a0655f1044
RUBA-SHREE <rubashree512@gmail.com>
2026-02-04 16:13:26 +0530

    non-overlapping intervals

---
+++ greedy algorithms/non-overlapping intervals.py
@@ -0,0 +1,14 @@
+class Solution:
+    def eraseOverlapIntervals(self, interval: List[List[int]]) -> int:
+        if not interval:
+            return 0
+        
+        interval.sort(key=lambda x:x[1])
+        c=1
+        last_end=interval[0][1]
+
+        for i in range(1,len(interval)):
+            if interval[i][0]>=last_end:
+                c+=1
+                last_end=interval[i][1]
+        return len(interval)-c
