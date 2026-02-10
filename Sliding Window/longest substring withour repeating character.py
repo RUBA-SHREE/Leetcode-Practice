d7f1e9a6126068789c83a1b52c9617568955a8de
RUBA-SHREE <rubashree512@gmail.com>
2026-02-10 10:15:13 +0530

    Longest substring wothour repeating characters

---
+++ Sliding Window/longest substring withour repeating character.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen=set()
+        left=0
+        ans=0
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left+=1
+            seen.add(s[right])
+            ans=max(ans,right-left+1)
+        return ans
+#Leetcode 3 Longest Substring Without Repeating Characters
