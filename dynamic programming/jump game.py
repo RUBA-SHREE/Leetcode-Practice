06f710c70a94261c0e5cbd2f959257cf98b11374
RUBA-SHREE <rubashree512@gmail.com>
2026-02-10 12:43:22 +0530

    jump game using dp

---
+++ dynamic programming/jump game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n=len(nums)
+        dp=[False]*n
+        dp[0]=True
+        for i in range(n):
+            if dp[i]:
+                for j in range(1,nums[i]+1):
+                    if i+j<n:
+                        dp[i+j]=True
+        return dp[-1]
+
+#Not optimal
+#just a practice of dynamic programming
+#Leetcode 55 Jump Game
