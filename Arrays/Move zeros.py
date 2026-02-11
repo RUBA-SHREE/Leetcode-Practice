7540c608ad9b9dedef10656fe93997afc2199d76
RUBA-SHREE <rubashree512@gmail.com>
2026-02-11 14:28:58 +0530

    Move zeros

---
+++ Arrays/Move zeros.py
@@ -0,0 +1,19 @@
+from typing import list
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i=0
+        for j in range(len(nums)):
+            if nums[j]!=0:
+                nums[i]=nums[j]
+                i+=1
+        while i<len(nums):
+            nums[i]=0
+            i+=1
+#Time:O(n)
+#space:O(1) 
+#Leetcode:283
+        
+        
